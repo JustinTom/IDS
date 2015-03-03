0b632bc1849b5893614738a313bd47f7550820c1
kayyjee <kylegilles@hotmail.com>
2015-03-03 00:57:45 -0800

    Block IP on SSH after failed attempts, Unblock after  a while
    Pretty much all functionality is completed.

--- ids.py
+++ ids.py
@@ -1,30 +1,25 @@
 """
+Steps for installing watchdog
 
 Step 1: Download pip at url below, call it get-pip.py
-
 https://bootstrap.pypa.io/get-pip.py
 
-
-
-
-Then run: 
-
-# python get-pip.py
-
-
-After install run: 
-
-
-# pip install watchdog
-
-
+Then run: # python get-pip.py
+Finally # pip install watchdog
 """
 
 #!/usr/bin/python
 import time
+import string
 import re
+import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from threading import Thread
+from time import sleep
+
+
+
 
 
 
@@ -32,11 +27,16 @@ class User(object):
     ip = ""
     timestampArray = []
 
+
     def __init__(self, ip, timestampArray):
         self.ip = ip
         self.timestampArray = timestampArray
 
-    
+
+
+
+
+
 
 def make_User(ip, timestampArray):
     user = User(ip, timestampArray)
@@ -44,7 +44,28 @@ def make_User(ip, timestampArray):
     
 
 def add_timestamp(user, timestamp):
-        user.timestampArray.append(timestamp)
+    user.timestampArray.append(timestamp)
+
+
+def block_User(user, IPADDRESS):
+    global banTime
+    print IPADDRESS + "Has been banned for " + str(banTime) + " seconds."
+    os.system("iptables -A INPUT -j DROP -p tcp --destination-port 22 -s"+ str(IPADDRESS))
+    thread = Thread(target = threaded_function, args = (user, IPADDRESS))
+    thread.start()
+    thread.join()
+
+def threaded_function(user, IPADDRESS):
+    global banTime
+    sleep(banTime)
+    os.system("iptables -D INPUT -j DROP -p tcp --destination-port 22 -s"+ str(IPADDRESS))
+    print ("Now Unblocking this user: " + IPADDRESS)
+
+
+def ip_toString(ip):
+    return str(ip).translate(string.maketrans('',''), '[]')
+
+
 
 
 
@@ -53,6 +74,10 @@ def add_timestamp(user, timestamp):
 
 class MyHandler(FileSystemEventHandler):
     global incorrectAttempts
+    global bannedIps
+    global numberOfAttempts
+
+
     def on_modified(self, event):
       
         if event.src_path == "/var/log/secure":
@@ -73,41 +98,31 @@ class MyHandler(FileSystemEventHandler):
                     add_timestamp(user, timestamp)
 
                     incorrectAttempts.append(user)
-                    print user.ip
-                    print user.timestampArray
-                    print "New User Added with that timestamp"
+                    print "Added New User"
+
+                    if len(user.timestampArray) >= numberOfAttempts:
+                        IPADDRESS = ip_toString(user.ip)
+                        block_User(user, IPADDRESS)
+
 
                 else:
                     
                     isnewuser = 0
-
                     for user in incorrectAttempts:  
                         if user.ip == ip:
                             if timestamp not in user.timestampArray:
-                                print "User already Exists, we added a timestamp to them"
-
-
-
-                                """
-                                    check if the timestampArray > 3
-                                        if it is:
-                                            block them
-
-                                            add them to the blockedusers array
-
-
-
-                                """
-
-
-
+                                print "User Already Exists, Adding Timestamp"
                                 add_timestamp(user, timestamp)
                                 print user.timestampArray
                                 isnewuser = 1
                                 
+                                if len(user.timestampArray) >= numberOfAttempts:
+                                    IPADDRESS = ip_toString(user.ip)
+                                    block_User(user, IPADDRESS)
+
 
                     if isnewuser == 0:   
-                        print "We Just added a new user"
+                        print "Added New User"
                         user = make_User(ip, timestampArray)
                         add_timestamp(user, timestamp)
                         incorrectAttempts.append(user)
@@ -116,21 +131,20 @@ class MyHandler(FileSystemEventHandler):
                         
                 
 
-
-
-
        
 
 if __name__ == "__main__":
+    banTime = input ("Enter how many seconds to ban an IP: ")
+    numberOfAttempts = input ("Enter how many fails until we ban an IP: ")
     event_handler = MyHandler()
     observer = Observer()
     observer.schedule(event_handler, path='/var/log', recursive=False)
     observer.start()
     incorrectAttempts = []
+    bannedIps = []
     try:
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
-
